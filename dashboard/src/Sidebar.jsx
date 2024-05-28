dafcf0a1a8274edc41ec24055ab165766fd84a84
urco <nik_186@hotmail.com>
2024-05-27 21:26:21 -0300

    sidebar

--- dashboard/src/Sidebar.jsx
+++ dashboard/src/Sidebar.jsx
@@ -1,10 +1,53 @@
 import React from "react";
+import { BsCart3,BsGrid1X2Fill,BsFillArchiveFill,BsFillGrid3X3GapFill, BsPeopleFill,BsListCheck, BsMenuButtonWideFill, BsFillGearFill } from "react-icons/bs";
 
 const Sidebar =()=>{
     return (
-        <div>
-           Sidebar
-        </div>
+        <aside id="sidebar">
+            <div className="sidebar-title">
+                <div className="sidebar-brand"> 
+                    <BsCart3 className="icon_header"/> SHOP
+                </div>
+                <span className="icon close_icon">X</span>
+            </div>
+             <ul className="sidebar-list">
+                <li className="sidebar-list-item">
+                    <a href="">
+                        <BsGrid1X2Fill className="icon"/> Dashboard
+                    </a>
+                </li>
+                <li className="sidebar-list-item">
+                    <a href="">
+                        <BsFillArchiveFill className="icon"/> Products
+                    </a>
+                </li>
+                <li className="sidebar-list-item">
+                    <a href="">
+                        <BsFillGrid3X3GapFill className="icon"/> Category
+                    </a>
+                </li>
+                <li className="sidebar-list-item">
+                    <a href="">
+                        < BsPeopleFill className="icon"/> Costumer
+                    </a>
+                </li>
+                <li className="sidebar-list-item">
+                    <a href="">
+                        < BsListCheck className="icon"/> Inventory
+                    </a>
+                </li>
+                <li className="sidebar-list-item">
+                    <a href="">
+                        <BsMenuButtonWideFill className="icon"/> Reports
+                    </a>
+                </li>
+                <li className="sidebar-list-item">
+                    <a href="">
+                        <BsFillGearFill className="icon"/> Settings
+                    </a>
+                </li>
+             </ul>
+        </aside>
     )
 }
 export default Sidebar;
