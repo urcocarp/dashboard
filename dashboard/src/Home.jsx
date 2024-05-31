29e11f0272fa81cd03f1957c5db560e37e799d32
urco <nik_186@hotmail.com>
2024-05-31 10:53:57 -0300

    source

--- dashboard/src/Home.jsx
+++ dashboard/src/Home.jsx
@@ -1,10 +1,51 @@
 import React from "react";
+import { BsCart3,BsGrid1X2Fill,BsFillArchiveFill,BsFillGrid3X3GapFill, BsPeopleFill,BsListCheck,
+     BsMenuButtonWideFill, BsFillGearFill, 
+     BsFillBellFill} from "react-icons/bs";
 
 const Home =()=>{
 return(
-    <div>
-      Home
-    </div>
+    <main className="main-container">
+      <div className="main-title">
+        <h3>DASHBOARD</h3>
+      </div>
+
+      <div className="main-cards">
+
+       <div className="card">
+          <div className="card-inner">
+           <h3>Products</h3>
+            <BsFillArchiveFill className="card_icon"/>
+           </div>
+          <h1>300</h1>
+        </div>
+
+        <div className="card">
+          <div className="card-inner">
+           <h3>Categories</h3>
+            <BsFillGrid3X3GapFill className="card_icon"/>
+           </div>
+          <h1>14</h1>
+        </div>
+
+        <div className="card">
+          <div className="card-inner">
+           <h3>Costumers</h3>
+            <BsPeopleFill className="card_icon"/>
+           </div>
+          <h1>37</h1>
+        </div>
+
+        <div className="card">
+          <div className="card-inner">
+           <h3>Alerts</h3>
+            <BsFillBellFill className="card_icon"/>
+           </div>
+          <h1>42</h1>
+        </div>
+
+      </div>
+    </main>
  )
 }
 
